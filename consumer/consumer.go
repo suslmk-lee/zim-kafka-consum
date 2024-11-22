e7dd102653a1f268cbf6be138ece8c2993643b37
이민규 <67575226+suslmk-lee@users.noreply.github.com>
2024-11-22 17:35:55 +0900

    update

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -23,11 +23,23 @@ func getEnv(key, fallback string) string {
 
 // CreateKafkaReader - Kafka 리더 설정 함수
 func CreateKafkaReader() *kafka.Reader {
+	broker := fmt.Sprintf("%s:%s", getEnv("KAFKA_HOST", config.KafkaHost), getEnv("KAFKA_PORT", config.KafkaPort))
+	topic := getEnv("KAFKA_TOPIC", config.KafkaTopic)
+	groupID := getEnv("KAFKA_GROUP_ID", config.GroupID)
+
+	log.Printf("Kafka Reader Configuration - Broker: %s, Topic: %s, GroupID: %s", broker, topic, groupID)
+
 	reader := kafka.NewReader(kafka.ReaderConfig{
-		Brokers: []string{fmt.Sprintf("%s:%s", getEnv("KAFKA_HOST", config.KafkaHost), getEnv("KAFKA_PORT", config.KafkaPort))},
-		Topic:   getEnv("KAFKA_TOPIC", config.KafkaTopic),
-		GroupID: getEnv("KAFKA_GROUP_ID", config.GroupID),
+		Brokers:  []string{broker},
+		Topic:    topic,
+		GroupID:  groupID,
+		MinBytes: 10e3, // 10KB
+		MaxBytes: 10e6, // 10MB
+		// 기타 설정 추가 가능
 	})
+
+	// Kafka 리더 생성 성공 로그
+	log.Println("Kafka reader successfully created and ready to consume messages")
 	return reader
 }
 
@@ -36,12 +48,14 @@ func ProcessKafkaMessage(reader *kafka.Reader, conn *pgx.Conn) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	// 메시지 읽기
+	// 메시지 읽기 시도 로그
+	log.Println("Attempting to read a message from Kafka...")
 	msg, err := reader.ReadMessage(ctx)
 	if err != nil {
 		log.Printf("Error reading message from Kafka: %v\n", err)
 		return
 	}
+	log.Printf("Message received: Partition=%d, Offset=%d, Key=%s", msg.Partition, msg.Offset, string(msg.Key))
 
 	// 메시지 디코딩
 	var data models.IoTData
@@ -50,6 +64,7 @@ func ProcessKafkaMessage(reader *kafka.Reader, conn *pgx.Conn) {
 		log.Printf("Error unmarshalling Kafka message: %v\n", err)
 		return
 	}
+	log.Printf("Message unmarshalled successfully: %+v", data)
 
 	// 최신 데이터 저장 (뮤텍스 사용)
 	models.SaveLatestData(data)
@@ -59,6 +74,6 @@ func ProcessKafkaMessage(reader *kafka.Reader, conn *pgx.Conn) {
 	if err != nil {
 		log.Printf("Error inserting data into DB: %v\n", err)
 	} else {
-		fmt.Printf("Data inserted into DB from Kafka at %v\n", data.Timestamp)
+		log.Printf("Data inserted into DB from Kafka at %v\n", data.Timestamp)
 	}
 }
